8fc63d31e864e5adc5eaadff4c8a032ef81610b0
Otávio Augusto <tavinteixeira@hotmail.com>
2022-01-26 19:05:26 -0300

    knew

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,14 +30,43 @@ interface HomeProps {
 }
 
 export default function Home({ postsPagination }: HomeProps) {
-  // const { next_page, result} = postsPagination
+  
+  const [nextPage, setNextPage] = useState(postsPagination.next_page)
+  const [posts, setPosts] = useState<Post[]>(postsPagination.result)
+
+  function handleShowMoreButton() {
+    fetch(nextPage).then((response) => 
+    response.json().then((json) => {
+      const outPutNewPosts = {
+        uid: json.results[0].uid,
+        first_publication_date: new Date(json.results[0].last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+        }),
+        data: {
+          title: RichText.asText(json.results[0].data.title),
+          subtitle: RichText.asText(json.results[0].data.subtitle),
+          author: RichText.asText(json.results[0].data.author),
+        }
+      }
+      console.log(json.results[0])
+      setPosts([...posts,outPutNewPosts])
+      // setNextPage(json.results[0].next_page)
+    }));
+  }
+
+  useEffect(() => {
+    
+  }, [posts,nextPage]);
+  
   return (
     <>
       <Head>
         <title>Home | SpaceTraveling</title>
       </Head>
       <main>
-        {/* {posts.map(post => (
+        {posts.map(post => (
           <Link href={`/posts/${post.uid}`}>
             <a key={post.uid}>
                 <time>{post.first_publication_date}</time>
@@ -46,7 +75,9 @@ export default function Home({ postsPagination }: HomeProps) {
                 <p>{post.data.subtitle}</p>
             </a>
           </Link>
-        )) */}
+        ))}
+
+        <button type="button" onClick={() => handleShowMoreButton()}>Mostrar mais</button>
       </main>
     </>
   )
@@ -59,8 +90,8 @@ export const getStaticProps: GetStaticProps = async () => {
     Prismic.predicates.at('document.type', 'posts')
   ],{
     fetch: ['posts.title', 'posts.subtitle','posts.author'],
-    pageSize: 1,
-    page: 1,
+    pageSize: 2,
+    page: 1 
   });
 
   const result = postsResponse.results.map(post => {
@@ -77,13 +108,17 @@ export const getStaticProps: GetStaticProps = async () => {
         author: RichText.asText(post.data.author),
       }
     }
-  })
+  });
 
+  console.log(postsResponse)
+
+  const next_page = postsResponse.next_page;
   
   return {
     props: {
       postsPagination: {
         result,
+        next_page
       },
     },
     revalidate: 60*60*24, // 24 horas
